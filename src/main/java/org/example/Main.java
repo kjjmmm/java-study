40be21d63b555e626e2e7207c7a0c1ce3a29e7e5
kjjmmm <kjjmmm123@naver.com>
2022-11-21 22:54:53 +0900

    카카오 뱅크 은행 설립 - 2022.11.21

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,40 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("test");
-    }
-    class Example{
-        private String letter;
+
+
+        Scanner sc = new Scanner(System.in);
+
+        while(true){
+
+            System.out.println("안녕하세요 카카오 뱅크입니다.");
+            System.out.println("어떤 업무를 보시겠습니까?");
+            System.out.println("1. 입금");
+            System.out.println("2. 출금");
+            System.out.println("3. 잔액 확인");
+            System.out.println("4. 종료");
+            int select_number = sc.nextInt();
+
+            if(select_number==1){
+                System.out.println("입금을 선택하였습니다.");
+            }else if(select_number==2){
+                System.out.println("출금을 선택하였습니다.");
+            }else if(select_number==3){
+                System.out.println("잔액 확인을 선택하였습니다.");
+            }else if(select_number==4){
+                System.out.println("안녕히 가십시오.");
+                break;
+            }
+
+        }
+
+
+
+
+
+
     }
 }
--- src/main/java/org/example/Sub.java
+++ src/main/java/org/example/Sub.java
@@ -2,4 +2,5 @@ package org.example;
 
 public class Sub {
     private String test;
+    
 }
